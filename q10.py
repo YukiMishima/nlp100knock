b7bfb44e1101d100316885b24e582f1ed853180f
yuki <amagai05.14@gmail.com>
2018-12-13 22:10:53 +0900

    delete q10~q14.py

--- q10.py
+++
@@ -1,2 +0,0 @@
-num_lines = sum(1 for line in open('hightemp.txt',encoding='utf-8'))
-print(num_lines)
--- q11.py
+++
@@ -1,4 +0,0 @@
-f = open('hightemp.txt',encoding='utf-8')
-for line in f.readlines():
-    print(line.replace('\t',' '))
-
