4c189a2e13f5fcaba8ec1f783bd834cb988edebb
yuki <amagai05.14@gmail.com>
2018-12-13 22:13:10 +0900

    deleted q15~19.py

--- q17.py
+++
@@ -1,8 +0,0 @@
-f = open('hightemp.txt',encoding='utf-8')
-prefecture_set=set()
-
-for line in f.readlines():
-    word_list = line.split()
-    prefecture_set.add(word_list[0])
-
-print(prefecture_set)
